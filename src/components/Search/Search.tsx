15ed4ebbff6f0e82bd35ff290107a33cdc5c903f
Andrew Jarrett <ahrjarrett@gmail.com>
2020-05-07 05:06:33 -0500

    using fp-ts to fetch ApolloData now

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,17 +1,40 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/react-hooks'
-import { functor } from 'fp-ts'
+import { ApolloError } from 'apollo-client'
 import { DocumentNode } from 'graphql'
+import * as E from 'fp-ts/lib/Either'
+import * as O from 'fp-ts/lib/Option'
+import { pipe } from 'fp-ts/lib/pipeable'
+import { ReadonlyRecord } from 'fp-ts/lib/ReadonlyRecord'
 
 interface Props {
   query: DocumentNode
 }
 
+type FetchData<Err, Data> = O.Option<E.Either<Err, Data>>
+
+// TODO: Define data type!
+type GithubData = ReadonlyRecord<string, any>
+
+type QueryState = FetchData<ApolloError, GithubData>
+
+const onError = (err: ApolloError) => <p>Error: {err.message}</p>
+const onLoading = () => <p>search results loading</p>
+const onSuccess = (data: GithubData) => <pre>{JSON.stringify(data, null, 2)}</pre>
+
 export const Search: React.FC<Props> = ({ query }) => {
-  const { loading, error, data } = useQuery<DocumentNode>(query)
+  const [queryState, setQueryState] = useState<QueryState>(O.none)
+  const handlers = {
+    onCompleted: (data: GithubData) => setQueryState(O.some(E.right(data))),
+    onError: (e: ApolloError) => setQueryState(O.some(E.left(e))),
+  }
 
-  console.log('functor', functor)
-  console.log('data', data)
+  useQuery<DocumentNode>(query, { ...handlers })
 
-  return loading ? <p>'search results loading'</p> : <pre>{JSON.stringify(data, null, 2)}</pre>
+  return (
+    <div>
+      Search results:
+      {pipe(queryState, O.fold(onLoading, E.fold(onError, onSuccess)))}
+    </div>
+  )
 }
